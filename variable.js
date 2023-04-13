54c04dc2a7b44a74ed64d21b6531773d6d3d68be
maithenkumar <maitheen1905@gmai.com>
2023-04-13 21:43:53 +0530

    adding typeof oprator

---
+++ variable.js
@@ -0,0 +1,18 @@
+let name="maithee"
+name = "jsdjfs"
+v = 9
+let age = 12
+var Age = 232
+age = 32;
+ var i = 0;
+while (i < v) {
+     age = 1;
+    // console.log(name);
+    Age=234
+    // console.log(i)
+    i++;
+}
+
+console.log(age);
+console.log(Age
+);
